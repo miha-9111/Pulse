180369876737eb5dd6606cc32ff537fdf0a5e0ee
miha-9111 <miha-9111@yandex.ru>
2020-04-13 23:34:27 +0300

    Pulse ver 1.9-add-jquery_validation_plugin

--- src/js/script.js
+++ src/js/script.js
@@ -50,4 +50,35 @@ $(document).ready(function(){
             $('.overlay, #order').fadeIn('slow');
         })
     });
+
+    function valideForms(form) {
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {  
+                    required: "Пожалуйства, введите свое имя",
+                    minlength: jQuery.validator.format("Введите {0} символа!")
+                },
+                phone: "Пожалуйста введите свой номер телефона",
+                email: {
+                    required: "Пожалуйста введите свой почту",
+                  email: "Неправильно введен адрес почты"
+                } 
+            }
+        });
+    };
+    
+    valideForms('#consultation-form');
+    valideForms('#consultation form');
+    valideForms('#order form');
 });
